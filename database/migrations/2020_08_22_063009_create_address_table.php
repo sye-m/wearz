f0288ab654eb747d4bd9999803d487facc67fdd5
midoriya-izuku <sairajmayekar12@gmail.com>
2020-08-22 21:36:27 +0530

    address table added

---
+++ database/migrations/2020_08_22_063009_create_address_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('street_name');
+            $table->string('street_address');
+            $table->string('landmark');
+            $table->string('state');
+            $table->string('zip_code');
+            $table->unsignedBigInteger('customer_id');
+            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('address');
+    }
+}
